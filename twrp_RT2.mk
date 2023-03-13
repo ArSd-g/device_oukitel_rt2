2ad9ab10167e098f26cd0cc39927c7f8ec62149b
laarSn <42816950+ArSd-g@users.noreply.github.com>
2023-03-13 14:05:41 +0100

    Update twrp_RT2.mk

--- twrp_RT2.mk
+++ twrp_RT2.mk
@@ -14,13 +14,13 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 # Inherit from RT2 device
 $(call inherit-product, device/wheatek/RT2/device.mk)
 
-PRODUCT_DEVICE := RT2
+PRODUCT_DEVICE := rt2
 PRODUCT_NAME := twrp_RT2
-PRODUCT_BRAND := OUKITEL
-PRODUCT_MODEL := RT2
-PRODUCT_MANUFACTURER := wheatek
+PRODUCT_BRAND := oukitel
+PRODUCT_MODEL := rt2
+PRODUCT_MANUFACTURER := oukitel
 
-PRODUCT_GMS_CLIENTID_BASE := android-wheatek
+PRODUCT_GMS_CLIENTID_BASE := android-oukitel
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="full_tp718_oq_p05_8788_s0_eea-user 12 SP1A.210812.016 1669003195 release-keys"
