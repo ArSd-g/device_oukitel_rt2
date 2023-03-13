3b544de2c04730205b14f45591195f72fa29375b
laarSn <42816950+ArSd-g@users.noreply.github.com>
2023-03-13 14:03:04 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/wheatek/RT2
+DEVICE_PATH := device/oukitel/rt2
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
